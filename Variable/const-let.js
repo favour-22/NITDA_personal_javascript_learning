f689f108c9d4ad701eb1b0866776d754d22d7643
favour-121 <anointed657@gmail.com>
2022-12-17 13:20:36 +0100

    data types

---
+++ Variable/const-let.js
@@ -0,0 +1,14 @@
+//making use of const and let keyword
+// A const keyword is a type of variable that cannot be changed
+
+const Apple = 5;
+const Ball = 6; 
+console.log(Apple)//5
+//if we try to redeclear it  will give us error
+/*Apple = 7;//SyntaxError: Identifier 'Apple' has already been declared
+console.log(Apple)*/
+
+//let as the same usage with the var keyword 
+let a = "apple"
+let b = "coke"
+console.log(`I love ${a} and i hate ${b} `)//I love apple and i hate coke 
--- Variable/user_input.js
+++ Variable/user_input.js
@@ -1,5 +1,5 @@
 /*Asking a user to enter a name in  javascript 
-1.we are going to make use of the const data type with cannot be changed when used
+1.we are going to make use of the const keyword with cannot be changed when used
 2.To ask a user to enter a number we make use of require on the command line;,but for the webrowser we use prompt() 
 */
 const readline = require("readline");
